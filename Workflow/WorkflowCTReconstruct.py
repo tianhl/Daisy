21a3c223509e9bed907190becee262b039073904
tianhl <tianhl@localhost.localdomain>
2021-07-06 16:57:11 +0800

    for 3W1 test workflow

--- Workflow/WorkflowCTReconstruct.py
+++ Workflow/WorkflowCTReconstruct.py
@@ -1,98 +1,76 @@
+
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # author: tianhl
 
 # using the new Workflow Engine for data analysis.
-     
-import Daisy 
+
+import Daisy
 
 init_dict   = {
-                'loadtomo':{'class_name':'LoadTIFs',\
+               'loadprojs':{'class_name':'DataHdlerAlg.LoadHDF5',\
                           },\
-                'loaddark':{'class_name':'LoadTIFs',\
+               'loaddarks':{'class_name':'DataHdlerAlg.LoadHDF5',\
                           },\
-                'loadflat':{'class_name':'LoadTIFs',\
+               'loadflats':{'class_name':'DataHdlerAlg.LoadHDF5',\
                           },\
-                'loadreco':{'class_name':'LoadHDF5',\
+            'filterdata':{'class_name':'PyAlgorithms.AlgSelectSinogram',\
                           },\
-             'filterdata':{'class_name':'AlgSelectSinogram',\
+              'normalize':{'class_name':'PyAlgorithms.AlgTomopyNormalize',\
                           },\
-              'normalize':{'class_name':'AlgTomopyNormalize',\
+                 'angles':{'class_name':'PyAlgorithms.AlgTomopyAngles',\
                           },\
-                'angles':{'class_name':'AlgTomopyAngles',\
+               'minuslog':{'class_name':'PyAlgorithms.AlgTomopyMinuslog',\
                           },\
-                'minuslog':{'class_name':'AlgTomopyMinuslog',\
+             'findcenter':{'class_name':'PyAlgorithms.AlgTomopyFindCenter',\
                           },\
-             'reconstruct':{'class_name':'AlgTomopyRecon',\
+            'reconstruct':{'class_name':'PyAlgorithms.AlgTomopyRecon',\
                           },\
-               'savedata':{'class_name':'SaveHDF5',\
-                          'init_paras':{'outputfile_name':'tomopy_scan.h5'}\
+               'savedata':{'class_name':'DataHdlerAlg.SaveTIFs',\
                           }\
               }
 
 
-cfg_dict    = {
-              'loadtomo':{'input_path':'/opt/CT/ZY-2/'},
-              'loaddark':{'input_path':'/opt/CT/ZY-2/'},
-              'loadflat':{'input_path':'/opt/CT/ZY-2/'},
-              'loadreco':{'inputfile_name':'/opt/CT/ZY-2/tomopy_reco.h5'}
-}
-#cfg_dict   = {'azintalg':{'directDist':169,\
-#                          'centerX':1049.967,\
-#                          'centerY':1063.892,\
-#                          'pixelX':75,\
-#                          'pixelY':75,\
-#                          'PlanRotation':64.66877,\
-#                          'tilt':0.3753,\
-#                          'azimin':93,\
-#                          'azimax':115,\
-#                          'radmin':10.0,\
-#                          'radmax':20,\
-#                          'ntth':40}\
-#             }
-#
-
-
 @Daisy.Base.Singleton
-class WorkflowCTReconstruct(Daisy.Base.PyWorkflow):
+class CTRecWorkflow(Daisy.Base.PyWorkflow):
+    def execute(self, projs_file, darks_file, flats_file, idx, initual_center, num_sample, output_name):
 
-    def execute(self):
-
-        pass
-        #self.engine['loadhdf5'].execute(input_path='/entry/tomo', output_dataobj='tomodata')
-        #self.engine['loadhdf5'].execute(input_path='/entry/dark', output_dataobj='darkdata')
-        #self.engine['loadhdf5'].execute(input_path='/entry/flat', output_dataobj='flatdata')
-        #self.engine['loadreco'].execute(input_path='/entry/reco', output_dataobj='recondata')
-        #self.engine['loadtomo'].execute(startswith='tomo_',seperator='_', idx=2, output_dataobj='tomodata')
-        #self.engine['loaddark'].execute(startswith='dark_',seperator='_', idx=1, output_dataobj='darkdata')
-        #self.engine['loadflat'].execute(startswith='flat_',seperator='_', idx=1, output_dataobj='flatdata')
-        #self.engine['filterdata'].execute(input_dataobj='tomodata', output_dataobj='tomodata_proc', idxs=[1,2])
-        #self.engine['filterdata'].execute(input_dataobj='darkdata', output_dataobj='darkdata_proc', idxs=[1,2])
-        #self.engine['filterdata'].execute(input_dataobj='flatdata', output_dataobj='flatdata_proc', idxs=[1,2])
-        #self.engine['normalize'].execute(projs_dataobj='tomodata_proc', darks_dataobj='darkdata_proc', flats_dataobj='flatdata_proc', output_dataobj='normdata')
-        #self.engine['normalize'].execute(projs_dataobj='tomodata', darks_dataobj='darkdata', flats_dataobj='flatdata', output_dataobj='normdata')
-        #self.engine['angles'].execute(input_dataobj='normdata', output_dataobj='thetas')
-        #self.engine['minuslog'].execute(input_dataobj='normdata', output_dataobj='mlogdata')
-        #self.engine['reconstruct'].execute(input_dataobj='mlogdata', theta='thetas',center=1030, alg_type='fbp', output_dataobj='recodata')
-        #self.engine['savedata'].execute(input_dataobj='tomodata', output_path='/entry/tomo' )
-        #self.engine['savedata'].execute(input_dataobj='darkdata', output_path='/entry/dark' )
-        #self.engine['savedata'].execute(input_dataobj='flatdata', output_path='/entry/flat' )
-        #self.engine['savedata'].execute(input_dataobj='recodata', output_path='/entry/reco' )
+        self.engine['loadprojs'].config(cfgdata=projs_file)
+        self.engine['loaddarks'].config(cfgdata=darks_file)
+        self.engine['loadflats'].config(cfgdata=flats_file)
+        self.engine['loadprojs'].execute(input_path='/scan/data/andor_img_shaped_image', output_dataobj='tomodata')
+        self.engine['loaddarks'].execute(input_path='/scan/data/andor_img_shaped_image', output_dataobj='darkdata')
+        self.engine['loadflats'].execute(input_path='/scan/data/andor_img_shaped_image', output_dataobj='flatdata')
+        self.engine['filterdata'].execute(input_dataobj='tomodata', output_dataobj='tomodata_proc', idxs=idx)
+        self.engine['filterdata'].execute(input_dataobj='darkdata', output_dataobj='darkdata_proc', idxs=idx)
+        self.engine['filterdata'].execute(input_dataobj='flatdata', output_dataobj='flatdata_proc', idxs=idx)
+        self.engine['normalize'].execute(projs_dataobj='tomodata_proc', darks_dataobj='darkdata_proc', flats_dataobj='flatdata_proc', output_dataobj='normdata')
+        self.engine['angles'].execute(input_dataobj='normdata', output_dataobj='thetas')
+        self.engine['minuslog'].execute(input_dataobj='normdata', output_dataobj='mlogdata')
+        self.engine['findcenter'].execute(input_dataobj='mlogdata', subpixel_accuracy = 0.5, initual_value= initual_center, output_dataobj='center')
+        center = self.get_data('center')
+        print('center: '+str(center))
+        for i in range(num_sample//2+1):
+            c=center-i*0.5
+            self.engine['reconstruct'].execute(input_dataobj='mlogdata', theta='thetas',center=c, alg_type='fbp', output_dataobj='recodata')
+            odata = self.get_data('recodata')
+            for i in range(odata.shape[0]):
+                self.engine['savedata'].execute(input_dataobj=odata[i], outputfile_name=output_name+str(c)+'_'+str(i)+'.tif' )
+        for i in range(num_sample//2):
+            c=center+(i+1)*0.5
+            self.engine['reconstruct'].execute(input_dataobj='mlogdata', theta='thetas',center=c, alg_type='fbp', output_dataobj='recodata')
+            odata = self.get_data('recodata')
+            for i in range(odata.shape[0]):
+                self.engine['savedata'].execute(input_dataobj=odata[i], outputfile_name=output_name+'_'+str(c)+'_'+str(i)+'.tif' )
 
 if __name__ == "__main__":
-    wf = WorkflowCTReconstruct('WorkflowCTReconstruct')
-    wf.setLogLevel(5)
-    #wf.setLogFile('log.log')
-    #wf.initialize(workflow_engine='PyWorkflowEngine', workflow_environment = init_dict, algorithms_cfg = cfg_dict)
-    wf.initialize(workflow_engine='PyWorkflowEngine', workflow_environment = init_dict)
-    wf.get_algorithm('loadtomo').config({'input_path':'/opt/CT/ZY-2/'})
-    wf.get_algorithm('loaddark').config({'input_path':'/opt/CT/ZY-2/'})
-    wf.get_algorithm('loadflat').config({'input_path':'/opt/CT/ZY-2/'})
-    wf.get_algorithm('loadreco').config({'inputfile_name':'/opt/CT/ZY-2/tomopy_reco.h5'})
-    wf.execute()
-    data =wf.data_keys()
-    algs =wf.algorithm_keys()
-    #wf.get_algorithm('loadmask')
-    #wf.get_data('result'))
+    projs_file = {'inputfile_name':'/hepsfs/bl/3W1/202106/Data/GB14-20210621-19/raw/crystal01/scan02/andor_img_tomo.h5'}
+    darks_file = {'inputfile_name':'/hepsfs/bl/3W1/202106/Data/GB14-20210621-19/raw/crystal01/scan02/andor_img_dark.h5'}
+    flats_file = {'inputfile_name':'/hepsfs/bl/3W1/202106/Data/GB14-20210621-19/raw/crystal01/scan02/andor_img_flat.h5'}
+    scratchdata='/hepsfs/bl/3W1/202106/Data/GB14-20210621-19/scratch/crystal01/scan02/'
 
+    wf = CTRecWorkflow('CTRecWorkflow')
+    wf.initialize(workflow_engine='PyWorkflowEngine', workflow_environment = init_dict)
+    wf.execute(projs_file, darks_file, flats_file, idx=[1,500], initual_center = 1012, num_sample=5, output_name=scratchdata+'rec_test')
     wf.finalize()
+
